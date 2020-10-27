db7ef7a35cc853e2d99d346577f2ecfe1e9d2da8
Rahul Saxena <saxenism@gmail.com>
2020-10-27 11:19:11 +0530

    Styled Join Page

--- server/index.js
+++ server/index.js
@@ -3,11 +3,23 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 3001;
-const router = require('./router.js');
+const router = require('./router.js'); //Our custom middleware
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!!");
+
+    socket.on('join', ({name, room}) => {
+        console.log(name, room);
+    })
+
+    socket.on('disconnect', () => {
+        console.log("User has left the chat");
+    });
+});
 
 app.use(router);
 
